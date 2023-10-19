8fdab30af753799f66b7c4524a4f99b5b8f2a7f2
unknown <visti@ymail.com>
2023-10-19 15:32:39 +0100

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+
+const elem = {
+  amount: document.querySelector('input[name="amount"]'),
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  btn: document.querySelector('.btn'),
+};
+
+let amount;
+let delay;
+
+elem.btn.addEventListener('click', submit);
+
+function submit(event) {
+  event.preventDefault();
+
+  amount = elem.amount.value;
+
+  for (let i = 1; i <= amount; i += 1) {
+    delay = Number(elem.delay.value) + Number(elem.step.value * (i - 1));
+    createPromise(i, delay);
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    setTimeout(() => {
+      Promise.resolve({ position: position, delay: delay }).then(value =>
+        Notiflix.Notify.success(`Fullfiled promis ${position} in ${delay}ms`)
+      );
+    }, delay);
   } else {
-    // Reject
+    setTimeout(() => {
+      Promise.reject({ position: position, delay: delay }).catch(value =>
+        Notiflix.Notify.failure(`Rejected promis ${position} in ${delay}ms`)
+      );
+    }, delay);
   }
 }
